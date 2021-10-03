4173b2d0ad14fe038aebaa42850eaecbae7d2a01
Cris <35613226+xmxvii@users.noreply.github.com>
2021-10-02 23:18:09 -0400

    Add files via upload

---
+++ src/Banner.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+
+function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+            request.data.results[
+                Math.floor(Math.random() * request.data.result.length - 1)
+            ]
+          ); // []
+          return request;
+        }
+        fetchData();
+    }, []);
+
+    console.log(movie);
+
+    return (
+        <header>
+            
+        </header>
+    );
+}
+
+export default Banner
